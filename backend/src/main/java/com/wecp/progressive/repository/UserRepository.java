02812ce211ff9a9ef30126cd01ed887a81184aaf
username <wecp-dummy@gmail.com>
2025-10-04 14:07:43 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.wecp.progressive.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wecp.progressive.entity.User;
+import java.util.List;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+    void deleteByPatientId(int patientId);
+    void deleteByDoctorId(int doctorId);
+}
